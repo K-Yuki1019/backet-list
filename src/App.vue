911c8af6d1ca3d0dcd7b6228407f0dedc98f697d
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-07-17 18:28:36 +0900

    delete(app): 不必要なComponent, VIewを削除

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Top</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
