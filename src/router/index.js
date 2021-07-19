2322c4be25c84ac3987824a0b67d3c79b63c3ca3
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-07-19 22:58:36 +0900

    firebase接続&ログイン画面のルーティング作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Top from "../views/Top.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Top",
     component: Top,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
