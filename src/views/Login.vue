ea06c3439401d9f3d6c00a7a64a0a5407c155add
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-07-24 00:09:19 +0900

    feat(Login): axiosを用いたログイン機能を実装

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="login">
-    <h1>これはログインページです</h1>
+    <h1>これはログインするためのページです</h1>
     <form class="login__form">
       <div class="login__input-item">
         <label for="email">メールアドレス</label>
-        <input type="email" />
+        <input type="email" id="email" v-model="email" />
+        <p v-show="email">{{ email }}</p>
       </div>
       <div class="login__input-item">
         <label for="password">パスワード</label>
-        <input type="password" />
+        <input type="password" id="passward" v-model="password" />
+        <p v-show="password">{{ password }}</p>
       </div>
-      <button class="login__button" @click="login()">ログイン</button>
+      <button class="login__button" type="button" @click="register()">
+        ログイン
+      </button>
     </form>
+    <router-link to="/">Topへ</router-link>
   </div>
 </template>
 <script>
+import axios from "../axios-for-auth.js";
 export default {
   name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
   methods: {
-    signUp: function() {},
+    register() {
+      axios
+        .post("/accounts:signUp?key=AIzaSyD1MbhRIeaeqS3wWpp9It--yDlawDee7xc", {
+          email: this.email,
+          password: this.password,
+          returnSecureToken: true,
+        })
+        .then((response) => {
+          console.log(response); //返ってきたレスポンスをログに表示
+        });
+      this.email = "";
+      this.password = "";
+    },
   },
 };
 </script>
